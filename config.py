2de344a29c950370b246b44318ee140864f599c8
Tony Vallad <anthony.vallad.cps@gmail.com>
2024-12-10 17:21:20 +0100

    Added color variables to change theme easily

--- config.py
+++ config.py
@@ -2,19 +2,6 @@ class Config:
     # Secret Key
     SECRET_KEY = 'your_secret_key'
     
-    # ANSI escape codes for colored output
-    GREEN = "\033[92m"
-    BLUE = "\033[94m"
-    YELLOW = "\033[93m"
-    RED = "\033[91m"
-    RESET = "\033[0m"  # White
-    
-    # Page Color Palette
-    # INF_COLOR = "#ad1888"
-    NOT_INF_COLOR = "#ad1838"
-    INF_COLOR = "#9A54B3"
-    # NOT_INF_COLOR = "#C60C31"
-    
     # Image settings
     IMG_SIZE = 128
     BATCH_SIZE = 32
@@ -23,3 +10,28 @@ class Config:
     MODEL_DIR = "src/static/model/"
     MODEL_NAME = 'model.keras'
     MODEL_PATH = MODEL_DIR + MODEL_NAME
+    
+    # COLOR SCHEME -------------------------------------------------------------------------------
+    
+    # ANSI escape codes for colored output
+    GREEN = "\033[92m"
+    BLUE = "\033[94m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    RESET = "\033[0m"  # White
+    
+    # Color Profile 1 - Red and Purple on Black background
+    NOT_INF_COLOR = "#AD1838"
+    # NOT_INF_COLOR = "#C60C31"  # Red from template website
+    # NOT_INF_COLOR = "#4CAF50"  # Green
+    # NOT_INF_COLOR = "#504CAF"  # Blue
+    INF_COLOR = "#9A54B3"
+    # BG_COLOR = "#050505"
+    BG_COLOR = "#050505"
+    TOP_BANNER_BG_COLOR = BG_COLOR
+    
+    # Color Profile 2
+    NOT_INF_COLOR = "#AD1838"  # Old green
+    INF_COLOR = "#9A54B3"
+    BG_COLOR = "#050505"
+    TOP_BANNER_BG_COLOR = BG_COLOR
